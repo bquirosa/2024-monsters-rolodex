efc2e034982cf4f895eb5f16bdf01626e41c1f8f
Braulio Quirós <braulio.quirosa@gmail.com>
2024-11-04 11:59:41 -0600

    States and Shallow Merge

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-class  App extends Component {
+class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: 'Monsters Rolodex'
-    }
-
+      name: {firstName: 'Braulio', lastName: 'Quiros'},
+      company: "Monster Inc Company",
+    };
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            {this.state.name} App
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
-          <button onClick={() => {
-            this.setState({
-              name: 'Monsters'
-            })
-          }}>Change Text</button>
+          <button
+            onClick={() => {
+              this.setState({
+                name: {firstName: 'Yurlin', lastName: 'Cordero'}
+              });
+              console.log(this.state)
+            }}
+          >
+            Change Text
+          </button>
         </header>
       </div>
     );
