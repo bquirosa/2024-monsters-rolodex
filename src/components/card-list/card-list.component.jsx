7df354decad9cf9e1e0aaf91f578fff9a7937e6d
Braulio Quirós <braulio.quirosa@gmail.com>
2024-11-08 08:55:25 -0600

    Finishing Touches

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,9 @@
 import { Component } from "react";
 
+import Card from "../card/card.component";
+
 import './card-list.styles.css'
+
 
 class CardList extends Component {
 
@@ -11,14 +14,8 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map(monster => {
-          const { name, email, id } = monster
-
           return (
-            <div className="card-container" key={id}>
-              <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
+            <Card monster={monster} key={monster.id}/>
           )
         })}
       </div>)
